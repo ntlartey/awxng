79cc373c0516647e1a7634f082b876e2382fc478
nik <nik.tettehlartey@gmail.com>
2022-03-20 14:38:26 +0100

    first commit

--- tools/docker-compose/start_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set +x
-
-cd /awx_devel
-make clean
-make awx-link
-
-cp tools/docker-compose/ansible/roles/sources/files/local_settings.py awx/settings/local_settings.py
-make "${1:-test}"
